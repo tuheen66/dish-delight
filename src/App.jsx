c5c49e98ae18d4999f6870f9a922077686f3871e
tuheen66 <hassan.monirul@gmail.com>
2024-05-12 03:11:37 +0600

    created banner and added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./Banner/Banner";
 import NavBar from "./NavBar/NavBar";
 
 function App() {
   return (
     <div className="container mx-auto">
       <NavBar></NavBar>
+      <Banner ></Banner>
     </div>
   );
 }
